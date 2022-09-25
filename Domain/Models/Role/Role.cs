d244776c5a79d2598026ecf5b24bc9d7a8784a67
mohammadHsoleymanpour <soley13822017@gmail.com>
2022-09-25 10:59:36 +0330

    test

--- Domain/Models/Role/Role.cs
+++ Domain/Models/Role/Role.cs
@@ -10,7 +10,7 @@ namespace Domain.Models.Role
     {
         public string RoleTitle { get; set; }
 
-
+        //
         #region Relations
 
         public List<UserRoles> UserRole { get; set; }

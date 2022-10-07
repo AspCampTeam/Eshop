72f2dee979a2649d0e759bb015f57ab49188cd28
Amirgh8080 <ghasemi.amir8080@gmail.com>
2022-10-07 23:07:27 +0330

    ErrorHandler Controller With Views

--- Eshop/Program.cs
+++ Eshop/Program.cs
@@ -37,26 +37,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/ErrorHandler/500");
     app.UseHsts();
 }
-else
-{
-    app.Use(async (ctx, next) =>
-    {
-        await next();
-        if (ctx.Response.StatusCode == 404 && !ctx.Response.HasStarted)
-        {
-            //Re-execute the request so the user gets the error page
-            ctx.Request.Path = "/404";
-            await next();
-        }
-    });
-}
 
-//app.UseStatusCodePagesWithRedirects("/{0}");
- 
+app.UseStatusCodePagesWithReExecute("/ErrorHandler/{0}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
